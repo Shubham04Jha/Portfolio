0bf9e168125678f3e1a1a0ade1d46270cd6dc86c
Shubham04Jha <shubhamjha70491@gmail.com>
2025-12-31 14:53:13 +0530

    widescreen footer css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,14 +7,14 @@ export const Footer = ()=>{
     const year = new Date().getFullYear();
     const {name,initials,githubLink,xLink,linkedinLink,firstName} = DATA;
     return(
-        <div className=" bg-secondary-950 text-lg min-h-20 flex flex-col gap-4 mb-4">
-            <p className="flex justify-center items-center">Designed and Developed by&nbsp;
+        <div className="  text-lg flex flex-col md:grid md:grid-cols-12 gap-2 py-4 ">
+            <p className="flex justify-center items-center col-span-5">Designed and Developed by&nbsp;
                 <Link to={'/about'} className="text-primary">{name}</Link>
             </p>
-            <p className="flex items-center justify-center">Copyright © {year} &nbsp;
+            <p className="flex items-center justify-center col-span-4">Copyright © {year} &nbsp;
                 <Link to={'/about'} className="text-primary">{initials}</Link>
             </p>
-            <div className='flex justify-center items-center gap-4 text-primary '>
+            <div className='flex justify-center items-center gap-4 text-primary col-span-3'>
                 <SocialIcon href={githubLink} label={`${firstName}'s github`} icon={FaGithub}/>
                 <SocialIcon href={xLink} label={`${firstName}'s x profile`} icon={FaXTwitter }/>
                 <SocialIcon href={linkedinLink} label={`${firstName}'s linkedin`} icon={FaLinkedinIn}/>
