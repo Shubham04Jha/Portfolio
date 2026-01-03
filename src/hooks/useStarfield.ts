019948e515d891a8b2ac56652372c514ad757b2c
Shubham04Jha <shubhamjha70491@gmail.com>
2026-01-03 11:18:24 +0530

    saving computations without constellation lines

--- src/hooks/useStarfield.ts
+++ src/hooks/useStarfield.ts
@@ -47,14 +47,10 @@ export const useStarField = (canvasRef: RefObject<HTMLCanvasElement|null>,starNu
             stars.forEach((star)=>{
                 star.phase += star.twinkleSpeed;
                 const currentOpacity = 0.2 + Math.abs(Math.sin(star.phase)) * 0.8; 
-
                 ctx.beginPath();
                 ctx.arc(star.x, star.y, star.size, 0, Math.PI*2);
-
                 ctx.fillStyle = `rgba(255, 255, 255, ${currentOpacity})`;
-
                 ctx.fill();
-
                 star.x+=star.vx;
                 star.y+=star.vy;
                 if(star.x<0||star.x>canvas.width){ 
@@ -67,25 +63,29 @@ export const useStarField = (canvasRef: RefObject<HTMLCanvasElement|null>,starNu
                     star.y = star.y<0?canvas.height:0; 
                     star.size=Math.random()*2;
                 }
-                const maxDistance = 100; // Only draw lines if stars are closer than 100px
-                for (let i = 0; i < stars.length; i++) {
-                    if(stars[i].size<0.1) continue;
-                    for (let j = i + 1; j < stars.length; j++) {
-                        if(stars[j].size<0.1) continue;
-                        const dx = stars[i].x - stars[j].x;
-                        const dy = stars[i].y - stars[j].y;
-                        const distance = Math.sqrt(dx * dx + dy * dy);
-                        if (distance < maxDistance&&Math.abs(stars[i].size-stars[j].size)<0.8) {
-                            const opacity = 1 - distance / maxDistance;
-                            ctx.strokeStyle = `rgba(235, 250, 250, ${opacity * 0.01})`;
-                            ctx.lineWidth = 0.5;
-                            ctx.beginPath();
-                            ctx.moveTo(stars[i].x, stars[i].y);
-                            ctx.lineTo(stars[j].x, stars[j].y);
-                            ctx.stroke();
+                const drawConstellationLines=()=>{
+                    const maxDistance = 100; // Only draw lines if stars are closer than 100px
+                    for (let i = 0; i < stars.length; i++) {
+                        if(stars[i].size<0.1) continue;
+                        for (let j = i + 1; j < stars.length; j++) {
+                            if(stars[j].size<0.1) continue;
+                            const dx = stars[i].x - stars[j].x;
+                            const dy = stars[i].y - stars[j].y;
+                            const distance = Math.sqrt(dx * dx + dy * dy);
+                            if (distance < maxDistance&&Math.abs(stars[i].size-stars[j].size)<0.8) {
+                                const opacity = 1 - distance / maxDistance;
+                                ctx.strokeStyle = `rgba(235, 250, 250, ${opacity * 0.01})`;
+                                ctx.lineWidth = 0.5;
+                                ctx.beginPath();
+                                ctx.moveTo(stars[i].x, stars[i].y);
+                                ctx.lineTo(stars[j].x, stars[j].y);
+                                ctx.stroke();
+                            }
                         }
                     }
                 }
+                // drawConstellationLines();
+                
             })
             animationFrameId = requestAnimationFrame(draw); // this is new...
         }
