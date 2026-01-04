6060ecda6e512e9806d84e01deaec35d49129e34
Shubham04Jha <shubhamjha70491@gmail.com>
2026-01-04 09:22:39 +0530

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import './App.css'
 import { Home } from './components/Home'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
-import { About } from './components/About'
+import { About } from './components/about/About'
 import { Background } from './components/Background'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <div className='text-text flex flex-col'>
+      <div className='text-text flex flex-col pt-20'>
         <Navbar />
         <div className='grow min-h-screen relative'>
           <Background/>
--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,10 @@ const DATA = {
     githubLink: "https://github.com/Shubham04Jha",
     linkedinLink: "https://www.linkedin.com/in/shubham-jha-85522528a/",
     leetcodeLink: "https://leetcode.com/u/shubham04jha/",
+    codeforcesLink: "https://codeforces.com/profile/Shubham_04Jha",
 }
+
+export const codeforcesLink = "https://codeforces.com/profile/Shubham_04Jha";
+export const codeforcesProfile= codeforcesLink.split("profile/")[1];
 
 export default DATA;
