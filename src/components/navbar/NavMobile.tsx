58f06646136239eae12a504f7723a30a7037576f
Shubham04Jha <shubhamjha70491@gmail.com>
2026-01-14 15:09:19 +0530

    removing portal to avoid stacking order issue in the content of navMobile

--- src/components/navbar/NavMobile.tsx
+++ src/components/navbar/NavMobile.tsx
@@ -29,47 +29,48 @@ export const NavMobile = ({NAV_ITEMS}:{NAV_ITEMS:{label: string, path: string, i
           transition: isDragging ? 'none' : 'all 0.2s ease-out',
           transform: 'translate(-60%,-50%)'
         }}
-        className={cn("fixed touch-none z-50 transition-transform ",isDragging&&"scale-120 cursor-grabbing","shadow-md shadow-accent/30")}
+        className={cn("fixed touch-none z-50 transition-transform ",
+          isDragging&&"scale-120 cursor-grabbing","shadow-md shadow-accent/30",
+          "outline-none"
+        )}
       >
         < ButtonIcon icon={MenuIcon} iconSize={36} className="bg-background/50 rounded-full backdrop-blur-md" />
       </DropdownMenu.Trigger>
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content 
-        side="left"
-        sideOffset={15}
-        className="min-w-50 bg-background-950/50 backdrop-blur-lg border border-primary/20 rounded-2xl p-2 shadow-lg shadow-accent/10 "
-        >
-          <DropdownMenu.RadioGroup value={selected} onValueChange={handleNavigation}>
-            {
-            NAV_ITEMS.map((item) =>
-              <DropdownMenu.RadioItem
-                key={item.path}
-                value={item.path}
-                className={cn("relative flex items-center gap-4 px-4 py-3 text-lg text-text-200 rounded-xl",selected===item.path&&'bg-primary/5 rounded-xl border border-primary/10')}
-              >
-                {/* Custom Indicator logic */}
-                <div className={cn("flex items-center justify-center w-6")}>
-                  <DropdownMenu.ItemIndicator>
-                    <FaDotCircle className="text-primary size-3 animate-pulse" />
-                  </DropdownMenu.ItemIndicator>
-                  {!selected.includes(item.path) && (
-                    <item.icon className="size-5 text-text-400 group-hover:text-primary transition-colors" />
-                  )}
-                </div>
-                <span className="font-medium tracking-wide">{item.label}</span>
-                
-                {/* Active Highlight Glow  I like this way of thinking!*/}
-                {/* {selected === item.value && (
-                  <div className="absolute inset-0 bg-primary/5 rounded-xl border border-primary/10 -z-10" />
-                )} */}
-              </DropdownMenu.RadioItem>
-            )
-            }
-          </DropdownMenu.RadioGroup>
+      <DropdownMenu.Content 
+      side="left"
+      sideOffset={15}
+      className="min-w-50 bg-background-950/50 backdrop-blur-lg border border-primary/20 rounded-2xl p-2 shadow-lg shadow-accent/10 "
+      >
+        <DropdownMenu.RadioGroup value={selected} onValueChange={handleNavigation}>
+          {
+          NAV_ITEMS.map((item) =>
+            <DropdownMenu.RadioItem
+              key={item.path}
+              value={item.path}
+              className={cn("relative flex items-center gap-4 px-4 py-3 text-lg text-text-200 rounded-xl",selected===item.path&&'bg-primary/5 rounded-xl border border-primary/10')}
+            >
+              {/* Custom Indicator logic */}
+              <div className={cn("flex items-center justify-center w-6")}>
+                <DropdownMenu.ItemIndicator>
+                  <FaDotCircle className="text-primary size-3 animate-pulse" />
+                </DropdownMenu.ItemIndicator>
+                {!selected.includes(item.path) && (
+                  <item.icon className="size-5 text-text-400 group-hover:text-primary transition-colors" />
+                )}
+              </div>
+              <span className="font-medium tracking-wide">{item.label}</span>
+              
+              {/* Active Highlight Glow  I like this way of thinking!*/}
+              {/* {selected === item.value && (
+                <div className="absolute inset-0 bg-primary/5 rounded-xl border border-primary/10 -z-10" />
+              )} */}
+            </DropdownMenu.RadioItem>
+          )
+          }
+        </DropdownMenu.RadioGroup>
 
-          <DropdownMenu.Arrow className="fill-primary/20" />
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
+        <DropdownMenu.Arrow className="fill-primary/20" />
+      </DropdownMenu.Content>
     </DropdownMenu.Root>
   );
 }
