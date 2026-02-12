e4abe43d4bedb444b8f8cb94f91ad43275f6c2db
Shubham04Jha <shubhamjha70491@gmail.com>
2026-02-12 22:40:58 +0530

    visitor count

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,6 +5,7 @@ import { Type } from "./Type";
 import { SocialSection } from "./SocialSection";
 import { Introduction } from "./Introduction";
 import { HeroImageCard } from "../ui/HeroImageCard";
+import { useVisitorCount } from "../../hooks/visitorCount";
 
 export const Home = ()=>{
     return <section className="px-4 py-8 max-w-5xl space-y-8 ">
@@ -14,12 +15,21 @@ export const Home = ()=>{
     </section>
 }
 
+const getOrdinalSuffix = (count: number)=>{
+    const s = ["th", "st", "nd", "rd"];
+    const v = count % 100;
+    return s[(v-20)%10] || s[v] || s[0]
+}
 const Hero = ()=>{
+    const {count} = useVisitorCount();
+    
     return(
         <div className="flex flex-wrap items-center gap-8">
             <div className=" md:flex-2 md:text-4xl text-3xl tracking-wide space-y-6 -mb-4">
                 <div className="flex gap-16 items-center">
-                    <h1 className="mt-3">Hi&nbsp; There!</h1>
+                    <h1 className="mt-3 text-xl">Hi There!
+                        {count?<span className="text-primary">{" "+count+getOrdinalSuffix(count)} visitor</span> :<></>}
+                    </h1>
                     <HandWave/>
                 </div>
                 <h1 className="heading-name tracking-widest leading-tight">
