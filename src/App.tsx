8758c8ad3b06c665e7e5d0b640227c8692f35473
Shubham04Jha <shubhamjha70491@gmail.com>
2026-02-24 20:46:19 +0530

    analytics vercel

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,31 @@ import { Reachout } from './components/Reachout'
 import { Projects } from './components/projects/Projects'
 import { Resume } from './components/Resume'
 import { Toast } from 'radix-ui'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
 
   return (
     <Toast.Provider>
-     <BrowserRouter>
-      <div className='text-text flex flex-col pt-20 min-h-dvh max-w-5xl mx-auto'>
-        <Navbar />
-        <Background/>
-        <div className='grow relative' >
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-            <Route path='/stars' element={<></>} />
-            <Route path='/reach-out' element={<Reachout />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/resume' element={<Resume />} />
-          </Routes>
+      <BrowserRouter>
+        <div className='text-text flex flex-col pt-20 min-h-dvh max-w-5xl mx-auto'>
+          <Navbar />
+          <Background/>
+          <div className='grow relative' >
+            <Routes>
+              <Route path='/' element={<Home/>} />
+              <Route path='/about' element={<About/>} />
+              <Route path='/stars' element={<></>} />
+              <Route path='/reach-out' element={<Reachout />} />
+              <Route path='/projects' element={<Projects />} />
+              <Route path='/resume' element={<Resume />} />
+            </Routes>
+          </div>
+          <Footer/>
         </div>
-        <Footer/>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
       <Toast.Viewport className="fixed bottom-8 right-4 flex flex-col min-w-xs max-w-xl m-0 list-none z-1000 outline-none" />
+      <Analytics/>
     </Toast.Provider>
   )
 }
